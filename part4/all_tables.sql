15e8e0a30ce7db271dd126ca70bdaf170ed822f7
eva983 <66435490+eva983@users.noreply.github.com>
2022-05-19 16:39:23 +0300

    Create all_tables.sql

---
+++ part4/all_tables.sql
@@ -0,0 +1,4 @@
+ALTER TABLE listings RENAME TO listing;
+ALTER TABLE reviews RENAME TO review;
+ALTER TABLE listings_summary RENAME TO listing_summary;
+ALTER TABLE reviews_summary RENAME TO review_summary;
